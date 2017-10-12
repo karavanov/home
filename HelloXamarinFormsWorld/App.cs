91be3a4e6822ec2568a1cf12dbdc8a350c9c3c55
mywestern <012345>
2017-10-12 19:06:38 +0300

    19-06

--- HelloXamarinFormsWorld/App.cs
+++ HelloXamarinFormsWorld/App.cs
@@ -10,7 +10,7 @@ namespace HelloXamarinFormsWorld
                    {
                        Content = new Label
                                  {
-                                     Text = "Hello, Forms myWestern! how a u? 123",
+                                     Text = "Hello, Forms myWestern! how a u? a",
                                      VerticalOptions = LayoutOptions.CenterAndExpand,
                                      HorizontalOptions = LayoutOptions.CenterAndExpand,
                                  },
